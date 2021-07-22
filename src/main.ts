df005e119b7c201f23039cccff6e93fa5414e41b
Erik August Johnson <erik@erikaugust.com>
2021-07-22 13:44:28 -0400

    Adds 'handlebars' templating

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  app.setViewEngine('handlebars');
   await app.listen(3000);
 }
 bootstrap();
